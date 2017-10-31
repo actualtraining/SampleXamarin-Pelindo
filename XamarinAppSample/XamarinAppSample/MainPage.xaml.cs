4e0eddfb6a480d2031cd311f4ec018d1a1c10abe
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-31 14:07:46 +0700

    menambahkan stepper

--- XamarinAppSample/XamarinAppSample/MainPage.xaml.cs
+++ XamarinAppSample/XamarinAppSample/MainPage.xaml.cs
@@ -26,6 +26,29 @@ namespace XamarinAppSample
             {
                 DisplayAlert("Keterangan","Anda memilih " + picker.Items[picker.SelectedIndex],"OK");
             };
+
+            datePicker.DateSelected += (object sender, DateChangedEventArgs e) =>
+            {
+                eventValue.Text = e.NewDate.ToString();
+                pageValue.Text = datePicker.Date.ToString();
+            };
+
+            timePicker.PropertyChanged += TimePicker_PropertyChanged;
+            stepper.ValueChanged += Stepper_ValueChanged;
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            eventValue.Text = String.Format("Stepper value is {0:F1}", e.NewValue);
+            pageValue.Text = stepper.Value.ToString();
+        }
+
+        private void TimePicker_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
+            {
+                pageValue.Text = timePicker.Time.ToString();
+            }
         }
 
         private void BtnAdd_Clicked(object sender, EventArgs e)
