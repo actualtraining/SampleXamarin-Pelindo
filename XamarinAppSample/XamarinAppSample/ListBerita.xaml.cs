b9eb537f82b2edeaa5a013c7a30ba8cd176ad098
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-31 15:42:50 +0700

    menambahkan custom list

--- XamarinAppSample/XamarinAppSample/ListBerita.xaml.cs
+++ XamarinAppSample/XamarinAppSample/ListBerita.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinAppSample.Models;
 
 namespace XamarinAppSample
 {
@@ -14,7 +15,26 @@ namespace XamarinAppSample
 	{
 		public ListBerita ()
 		{
-			InitializeComponent ();
-		}
-	}
+			InitializeComponent();
+
+            List<Berita> lstBerita = new List<Berita>()
+            {
+                new Berita {Judul="Memulai Startup Bisnis",
+                    Keterangan = "Berita berisi bagaimana memulai startup bisnis dan membuat valuasi awal" },
+                new Berita {Judul="Xamarin for Android",
+                    Keterangan="Xamarin for Android digunakan jika anda menginginkan tampilan yang sangat flexible menggunakan native xml android"},
+                new Berita {Judul="Xamarin Forms",Keterangan="Xamarin Forms digunakan untuk membuat aplikasi dengan 100% code sharing di C#"}
+            };
+
+            listBerita.ItemsSource = lstBerita;
+
+            listBerita.ItemTapped += (object sender, ItemTappedEventArgs e) =>
+            {
+                Berita currBerita = e.Item as Berita;
+                DisplayAlert("Keterangan", "Judul " + currBerita.Judul, "OK");
+            };
+        }
+
+        
+    }
 }
