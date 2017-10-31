e45ca830c763bfd5df044baab843585fa6bdfc32
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-31 13:43:18 +0700

    Add project files.

---
+++ XamarinAppSample/XamarinAppSample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinAppSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnAdd.Clicked += BtnAdd_Clicked;
+
+            //sample picker
+            var options = new List<string> { "Satu", "Dua", "Tiga", "Empat", "Lima" };
+           
+            foreach(var option in options)
+            {
+                picker.Items.Add(option);
+            }
+        }
+
+        private void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Pesan", "Hallo " + txtHello.Text, "OK");
+        }
+
+        private void btnHello_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hello", "Hello World", "OK");
+        }
+    }
+}
