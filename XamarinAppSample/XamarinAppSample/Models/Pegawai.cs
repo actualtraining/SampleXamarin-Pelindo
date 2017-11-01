b07f920f9936bf327f7a6fe3acc473bd796e214b
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-01 10:52:59 +0700

    menambahkan data binding di model

--- XamarinAppSample/XamarinAppSample/Models/Pegawai.cs
+++ XamarinAppSample/XamarinAppSample/Models/Pegawai.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace XamarinAppSample.Models
 {
-    public class Pegawai
+    public class Pegawai : BindableObject
     {
-        public string Nip { get; set; }
-        public string Nama { get; set; }
-        public string Email { get; set; }
-        public string Pic { get; set; }
+        private string _nip;
+        public string Nip
+        {
+            get { return _nip; }
+            set { _nip = value; OnPropertyChanged("Nip"); }
+        }
+
+        private string _nama;
+        public string Nama
+        {
+            get { return _nama; }
+            set { _nama = value; OnPropertyChanged("Nama"); }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged("Email"); }
+        }
+
+        private string _pic;
+        public string Pic
+        {
+            get { return _pic; }
+            set { _pic = value; OnPropertyChanged("Pic"); }
+        }
     }
 }
