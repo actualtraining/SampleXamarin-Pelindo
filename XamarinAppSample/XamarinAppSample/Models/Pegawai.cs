76e28caef448111a12f170dc7e03e5e3b5263631
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-01 08:43:17 +0700

    menambahkan list pegawai

---
+++ XamarinAppSample/XamarinAppSample/Models/Pegawai.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinAppSample.Models
+{
+    public class Pegawai
+    {
+        public string Nip { get; set; }
+        public string Nama { get; set; }
+        public string Email { get; set; }
+        public string Pic { get; set; }
+    }
+}
