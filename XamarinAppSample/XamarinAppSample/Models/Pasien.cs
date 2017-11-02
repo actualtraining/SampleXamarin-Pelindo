6bbd32beaa1b42135fe198beaf00e90e4edfa348
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-02 15:43:04 +0700

    menambahkan http client

---
+++ XamarinAppSample/XamarinAppSample/Models/Pasien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinAppSample.Models
+{
+    public class Pasien
+    {
+        public int PasienID { get; set; }
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public string Telpon { get; set; }
+        public int Umur { get; set; }
+    }
+}
---
+++ XamarinAppSample/XamarinAppSample/Services/PasienService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace XamarinAppSample.Services
+{
+    public class PasienService
+    {
+        private HttpClient _client;
+        public PasienService()
+        {
+            _client = new HttpClient();
+        }
+    }
+}
