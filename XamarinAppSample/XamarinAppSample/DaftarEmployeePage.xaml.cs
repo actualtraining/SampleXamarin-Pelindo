babe05adca695e7eed01c76bb5858101b9cb3563
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-02 10:45:10 +0700

    select employee

---
+++ XamarinAppSample/XamarinAppSample/DaftarEmployeePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinAppSample.Data;
+using XamarinAppSample.Models;
+
+namespace XamarinAppSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DaftarEmployeePage : ContentPage
+    {
+        private DataAccess dataAccess;
+        public DaftarEmployeePage()
+        {
+            InitializeComponent();
+            dataAccess = new DataAccess();
+        }
+
+        protected override void OnAppearing()
+        {
+            var data = dataAccess.GetAll();
+            myListView.ItemsSource = data;
+        }
+    }
+}
