8f95abccd103f75cc67c3d586c796b28de93bb88
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-02 10:37:52 +0700

    menambahkan data access

---
+++ XamarinAppSample/XamarinAppSample/Data/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite.Net;
+using Xamarin.Forms;
+using XamarinAppSample.Models;
+
+namespace XamarinAppSample.Data
+{
+    public class DataAccess
+    {
+        SQLiteConnection dbConn;
+        public DataAccess()
+        {
+            dbConn = DependencyService.Get<ISQLIte>().GetConnection();
+            dbConn.CreateTable<Employee>();
+        }
+
+        public List<Employee> GetAll()
+        {
+            return dbConn.Query<Employee>("select * from Employee order by EmpName");
+        }
+
+        public int Insert(Employee employee)
+        {
+            return dbConn.Insert(employee);
+        }
+
+        public int Update(Employee employee)
+        {
+            return dbConn.Update(employee);
+        }
+
+        public int Delete(Employee employee)
+        {
+            return dbConn.Delete(employee);
+        }
+    }
+}
