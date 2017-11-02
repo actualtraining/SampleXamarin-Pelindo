d15d7f0b692722cd9453e839765309537a8cc3bf
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-02 15:49:48 +0700

    menambahkan services

--- XamarinAppSample/XamarinAppSample/Services/PasienService.cs
+++ XamarinAppSample/XamarinAppSample/Services/PasienService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using System.Net.Http;
 using Newtonsoft.Json;
+using XamarinAppSample.Models;
 
 namespace XamarinAppSample.Services
 {
@@ -15,6 +16,22 @@ namespace XamarinAppSample.Services
         public PasienService()
         {
             _client = new HttpClient();
+        }
+
+        public async Task<List<Pasien>> GetAll()
+        {
+            var strUri = "http://pelindo3.azurewebsites.net/api/Pasien";
+            var response = await _client.GetAsync(strUri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<Pasien>>(content);
+                return data;
+            }
+            else
+            {
+                throw new Exception(response.StatusCode.ToString());
+            }
         }
     }
 }
